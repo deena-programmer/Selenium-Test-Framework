9fcb4cb6998f70fabd6a10ce6bf692c7e72f5490
deena-programmer <deenak2896@gmail.com>
2026-08-06 11:59:41 +0530

    test checking

--- src/test/java/com/orangeHRM/test/DummyClass.java
+++ src/test/java/com/orangeHRM/test/DummyClass.java
@@ -1,6 +1,5 @@
 package com.orangeHRM.test;
 
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.orangeHRM.base.BaseClass;
@@ -10,13 +9,13 @@ public class DummyClass extends BaseClass{
 
 	@Test
 	public void dummyTest() {
-		//ExtentManager.startTest("DummyTest Test"); --This has been implemented in TestListener
-		String title = getDriver().getTitle();
-		ExtentManager.logStep("Verifying the title");
-		assert title.equals("OrangeHRM") : "Test Failed - Title is not matching";
-		
-		System.out.println("Test Passed - Title is matching");
-		ExtentManager.logSkip("This case is skipped");
-		throw new SkipException("Skipping the test as part of testing");
+		//Test checking
+	//ExtentManager.startTest("DummyTest Test"); --This has been implemented in TestListener
+			String title = getDriver().getTitle();
+	ExtentManager.logStep("Verifying the title");
+	assert title.equals("OrangeHRM") : "Test Failed - Title is not matching";
+	
+	System.out.println("Test Passed - Title is matching");
+	ExtentManager.logStep("Validation successful");
 	}
 }
